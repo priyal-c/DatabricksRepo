86ac1695260dcc988a5e975c76143e48911f224c
priyal-c <priyal-c>
2021-09-23 14:11:12 +0000

    checkin

---
+++ Number_package.scala.scala
@@ -0,0 +1,7 @@
+// Databricks notebook source
+package x.y.z2
+
+object Utils {
+  val aNumber = 5 // works!
+  def functionThatWillWork(a: Int): Int = a + 1
+}
---
+++ package_test.scala.scala
@@ -0,0 +1,6 @@
+// Databricks notebook source
+// MAGIC %run ./Number_package
+
+// COMMAND ----------
+
+import x.y.z2.Utils
